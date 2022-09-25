6688fc8b42ff0602d7eaf6ff0da00ee8aa454c2b
Areeb Khan <39133435+AreebKhan619@users.noreply.github.com>
2022-09-25 21:45:31 +0530

    Time: 132 ms (52.52%), Space: 51.7 MB (67.24%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -9,13 +9,10 @@ var maxProfit = function (prices) {
 
   while (rightIdx < prices.length) {
     // if at any moment we come across a value in the next day where the price is lower, we can put the 'buy' time for that day.
-    const priceForSell = prices[rightIdx];
-    const priceForBuy = prices[leftIdx];
-
-    if (priceForSell < priceForBuy) {
+    if (prices[rightIdx] <  prices[leftIdx]) {
       leftIdx = rightIdx;
     } else {
-      profit = Math.max(priceForSell - priceForBuy, profit);
+      profit = Math.max(prices[rightIdx] - prices[leftIdx], profit);
     }
     rightIdx++;
   }
