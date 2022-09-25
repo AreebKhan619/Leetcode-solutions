7cb399f3611e606f20f971ffed6e3e548dfa5086
Areeb Khan <39133435+AreebKhan619@users.noreply.github.com>
2022-09-26 00:05:18 +0530

    Time: 63 ms (99.45%), Space: 45.9 MB (52.43%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -4,6 +4,7 @@
  */
 var isPalindrome = function (s) {
   s = s.replaceAll(/[\W_]+/g, "").toLowerCase();
+    return s === Array.from(s).reverse().join("");
   const len = s.length - 1;
   for (const idx in s) {
     if (s.charAt(idx) !== s.charAt(len - idx)) return false;
