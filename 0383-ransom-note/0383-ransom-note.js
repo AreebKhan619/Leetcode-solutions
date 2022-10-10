89386846fd79464eda20bc3bb81cf2e2ad151e10
Areeb Khan <39133435+AreebKhan619@users.noreply.github.com>
2022-10-10 19:36:08 +0530

    Time: 196 ms (10.39%), Space: 49.1 MB (11.51%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+  const mapping = {};
+
+  for (const i of ransomNote) {
+    mapping[i] = (mapping[i] || 0) + 1;
+  }
+
+  for (const i of magazine) {
+    if (mapping[i] === 1) delete mapping[i];
+    else if (mapping[i]) mapping[i] = mapping[i] - 1;
+    if (!Object.keys(mapping).length) return true;
+  }
+
+  return false;
+};
