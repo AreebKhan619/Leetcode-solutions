89a4af55582808efc0e15e40ee8bc6dd0f58b64f
Areeb Khan <39133435+AreebKhan619@users.noreply.github.com>
2022-10-13 10:30:32 +0530

    Time: 97 ms (38.40%), Space: 42.1 MB (29.52%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  let secondLast = 0;
+  let last = 1;
+  let i = 1;
+  while (i <= n) {
+    let ways = 0;
+    if (i - 2 >= 0) {
+      ways += secondLast;
+    }
+    if (i - 1 >= 0) {
+      ways += last;
+    }
+    if (i === n) return ways;
+    secondLast = last;
+    last = ways;
+    i++;
+  }
+  return last;
+};
