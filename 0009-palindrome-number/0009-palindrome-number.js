9fe39cba4a38ad26c5e78cbe560fe7cca03a0cba
Areeb Khan <39133435+AreebKhan619@users.noreply.github.com>
2022-10-21 00:31:10 +0530

    Time: 299 ms (59.91%), Space: 50.9 MB (64.12%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -6,7 +6,7 @@ var isPalindrome = function(x) {
     x = x.toString();
     let len=x.length, i;
     for(i=0;i<len;i++){
-        if(x[i]!==x[len - i -1])return false
+     if(x[i]!==x[len - i -1])return false
     }
     return true
 };
