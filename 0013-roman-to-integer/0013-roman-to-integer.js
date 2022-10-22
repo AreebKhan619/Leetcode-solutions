6341226674f0dc6eafbc30c53eda4fef1b4af0dc
Areeb Khan <39133435+AreebKhan619@users.noreply.github.com>
2022-10-22 21:20:40 +0530

    Time: 146 ms (86.83%), Space: 50.6 MB (10.88%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,38 @@
+const getVal = (s) => {
+  switch (s) {
+    case "I":
+      return 1;
+    case "V":
+      return 5;
+    case "X":
+      return 10;
+    case "L":
+      return 50;
+    case "C":
+      return 100;
+    case "D":
+      return 500;
+    case "M":
+      return 1000;
+    default:
+      return 0;
+  }
+};
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  let total = 0,
+    prevVal = 0;
+  for (const i in s) {
+    let val = getVal(s[i]);
+    total += val;
+    if (prevVal < val) {
+      total -= 2 * prevVal;
+    }
+    prevVal = val;
+  }
+  return total;
+};
