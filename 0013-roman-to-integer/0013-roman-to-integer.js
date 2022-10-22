faa3d8f52838ae1fe91017e47fb11de3a3e69e01
Areeb Khan <39133435+AreebKhan619@users.noreply.github.com>
2022-10-22 21:23:48 +0530

    Time: 275 ms (14.51%), Space: 51.3 MB (9.04%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -1,22 +1,11 @@
-const getVal = (s) => {
-  switch (s) {
-    case "I":
-      return 1;
-    case "V":
-      return 5;
-    case "X":
-      return 10;
-    case "L":
-      return 50;
-    case "C":
-      return 100;
-    case "D":
-      return 500;
-    case "M":
-      return 1000;
-    default:
-      return 0;
-  }
+const map = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
 };
 
 /**
@@ -24,10 +13,10 @@ const getVal = (s) => {
  * @return {number}
  */
 var romanToInt = function (s) {
-  var total = 0,
-    prevVal = 0, val;
+  let total = 0,
+    prevVal = 0;
   for (const i in s) {
-    val = getVal(s[i]);
+    let val = map[s.charAt(i)];
     total += val;
     if (prevVal < val) {
       total -= 2 * prevVal;
