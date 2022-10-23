879069adc7079197ee7eafdac95c910f16a5d15e
Areeb Khan <39133435+AreebKhan619@users.noreply.github.com>
2022-10-24 02:09:50 +0530

    Time: 161 ms (29.97%), Space: 51.3 MB (5.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -7,11 +7,11 @@ var isAnagram = function (s, t) {
   if (s.length !== t.length) return false;
   const map = {};
   for (const i in s) {
-    const letter = s.charAt(i);
+    const letter = s[i];
     map[letter] = (map[letter] || 0) + 1;
   }
   for (const i in t) {
-    const letter = t.charAt(i);
+    const letter = t[i];
     if (!map[letter]) return false;
     else if (map[letter] === 1) delete map[letter];
     else map[letter] = map[letter] - 1;
