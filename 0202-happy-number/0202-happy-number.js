f5f45a1f1fc8f07d6c41fff2a36072fbf6101e35
Areeb Khan <39133435+AreebKhan619@users.noreply.github.com>
2022-10-25 11:22:05 +0530

    Time: 83 ms (82.18%), Space: 47.9 MB (5.02%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function (n) {
+  const map = {};
+  let num = n.toString();
+
+  while (!map[num]) {
+    let sum = 0;
+    for (const idx in num) {
+      sum += num[idx] * num[idx];
+    }
+    if (sum === 1) return true;
+    map[num] = true;
+    num = sum.toString();
+  }
+  return false;
+};
