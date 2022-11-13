32a22b1d807d29894b0eeaeaee17ab89ab49f1ce
Areeb Khan <39133435+AreebKhan619@users.noreply.github.com>
2022-11-13 20:45:29 +0530

    Time: 111 ms (68.02%), Space: 43.8 MB (52.97%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
@@ -6,9 +6,8 @@ var titleToNumber = function (columnTitle) {
   let sum = 0,
     len = columnTitle.length;
   for (let index = len; index > 0; index--) {
-    const codeNo = columnTitle.charCodeAt(index - 1) - 64;
-    const stepNo = len - index;
-    sum += Math.pow(26, stepNo) * codeNo;
+    var codeNo = columnTitle.charCodeAt(index - 1) - 64;
+    sum += Math.pow(26, len-index) * codeNo;
   }
   return sum;
 };
