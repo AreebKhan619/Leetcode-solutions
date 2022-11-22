c2ab985bbf99cceb0e0151a4b966933cefee6bb4
Areeb Khan <39133435+AreebKhan619@users.noreply.github.com>
2022-11-22 20:38:44 +0530

    Time: 121 ms (51.85%), Space: 48.6 MB (13.26%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -4,10 +4,11 @@
  */
 var singleNumber = function(arr) {
   const pairMap = new Map();
-        
-  arr.forEach(el => {
-      pairMap.has(el) ? pairMap.delete(el) : pairMap.set(el, true)
-  });
+   
+  for(el of arr){
+       pairMap.has(el) ? pairMap.delete(el) : pairMap.set(el, true)
+  }  
+
 
   return pairMap.keys().next().value
 };
