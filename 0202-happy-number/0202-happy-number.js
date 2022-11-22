e256086797bbcf8a5b2b9a36ba76376f8887bf8e
Areeb Khan <39133435+AreebKhan619@users.noreply.github.com>
2022-11-22 20:52:07 +0530

    Time: 133 ms (14.10%), Space: 48.6 MB (5.11%) - LeetHub

--- 0202-happy-number/0202-happy-number.js
+++ 0202-happy-number/0202-happy-number.js
@@ -3,17 +3,17 @@
  * @return {boolean}
  */
 var isHappy = function (n) {
-  const map = {};
-  var num = n.toString();
-  var sum, idx
-  while (!map[num]) {
-    sum = 0;
-    for (idx in num) {
-      sum += Math.pow(num[idx], 2);
+  const s = new Set();
+  const getSumOfSquares = (num) => {
+    if (s.has(num)) return false;
+    let sum = 0;
+    for (const idx in num) {
+      sum += num[idx] * num[idx];
     }
     if (sum === 1) return true;
-    map[num] = true;
-    num = sum.toString();
-  }
-  return false;
+    s.add(num);
+    return getSumOfSquares(sum + "");
+  };
+
+  return getSumOfSquares(n + "");
 };
