6a8ebc6c3cecfdb59ed0a8ce1622e10222adc27f
Areeb Khan <39133435+AreebKhan619@users.noreply.github.com>
2022-11-27 01:44:39 +0530

    Time: 60 ms (99.82%), Space: 44.1 MB (53.09%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.js
+++ 1089-duplicate-zeros/1089-duplicate-zeros.js
@@ -3,14 +3,12 @@
  * @return {void} Do not return anything, modify arr in-place instead.
  */
 var duplicateZeros = function (arr) {
-  const len = arr.length;
   const newArr = [];
-
   arr.forEach((el) => {
     if (!el) newArr.push(0, 0);
     else newArr.push(el);
   });
-  newArr.length = len;
+  newArr.length = arr.length;
   arr.length = 0;
   arr.push(...newArr);
 };
