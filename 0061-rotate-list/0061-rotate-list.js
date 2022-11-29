8a9e5aa763dca4cccd798478bf1558910a23d698
Areeb Khan <39133435+AreebKhan619@users.noreply.github.com>
2022-11-29 16:04:34 +0530

    Time: 135 ms (15.24%), Space: 43.4 MB (95.55%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.js
@@ -0,0 +1,33 @@
+var rotateRight = function (head, k) {
+  let length = 0;
+  let currentNode = head;
+
+  while (currentNode) {
+    length++;
+    currentNode = currentNode.next;
+  }
+  if (length === 1 || length === k) return head;
+
+  if (k > length) {
+    k = k % length;
+  }
+  if (!head || !k) return head;
+
+  let currLen = 0;
+  currentNode = head;
+  let nodeNumberToDetach = length - k;
+  let newHead;
+  while (currLen < nodeNumberToDetach - 1 && currentNode) {
+    currLen++;
+    currentNode = currentNode.next;
+  }
+  newHead = currentNode.next;
+  currentNode.next = null;
+  currentNode = newHead;
+  while (currentNode?.next) {
+    currentNode = currentNode.next;
+  }
+  currentNode.next = head;
+
+  return newHead;
+};
