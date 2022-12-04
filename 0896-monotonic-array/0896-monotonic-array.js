aa074a71cd0d3c52a51bb120266d4903fea10c8a
Areeb Khan <39133435+AreebKhan619@users.noreply.github.com>
2022-12-04 23:48:06 +0530

    Time: 202 ms (13.87%), Space: 53.6 MB (33.94%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.js
+++ 0896-monotonic-array/0896-monotonic-array.js
@@ -1,20 +1,56 @@
+  const isUndefined = (sth) => sth === undefined;
 /**
  * @param {number[]} nums
  * @return {boolean}
  */
 var isMonotonic = function (nums) {
-  let idx = 0;
+  let leftIdx = 0,
+    rightIdx = nums.length - 1;
+
   let isIncreasing;
-  while (idx < nums.length) {
-    if (isIncreasing !== undefined) {
-      if (isIncreasing && nums[idx] < nums[idx - 1]) return false;
-      if (!isIncreasing && nums[idx] > nums[idx - 1]) return false;
-    } else {
-      if (idx > 0 && nums[idx] !== nums[idx - 1]) {
-        isIncreasing = nums[idx] > nums[idx - 1];
+  let prevLeft = nums[leftIdx],
+    prevRight = nums[rightIdx];
+
+
+  while (leftIdx < nums.length && rightIdx > -1) {
+    if (isUndefined(isIncreasing)) {
+      let isLeftIncreasing, isRightIncreasing;
+      if (nums[leftIdx] !== prevLeft) {
+        isLeftIncreasing = nums[leftIdx] > prevLeft;
+      }
+      if (nums[rightIdx] !== prevRight) {
+        isRightIncreasing = nums[rightIdx] < prevRight;
+      }
+
+      // if both are undefined, do nothing
+      // if either one is defined, keep that value
+      // if both are defined, check if they match, if they don't return false, else keep either value since they match
+
+      const isLeftDefined = !isUndefined(isLeftIncreasing);
+      const isRightDefined = !isUndefined(isRightIncreasing);
+
+      if (isLeftDefined || isRightDefined) {
+        if (isLeftDefined && isRightDefined) {
+          if (isLeftIncreasing !== isRightIncreasing) return false;
+          isIncreasing = isLeftIncreasing;
+        } else {
+          isIncreasing = isLeftIncreasing ?? isRightIncreasing;
+        }
       }
     }
-    idx++;
+
+    if (isIncreasing && prevLeft > nums[leftIdx]) return false;
+    if (!isIncreasing && nums[leftIdx] > prevLeft) return false;
+
+    if (isIncreasing && prevRight < nums[rightIdx]) return false;
+    if (!isIncreasing && prevRight > nums[rightIdx]) return false;
+
+    if (leftIdx === rightIdx + 1) return true;
+
+    prevLeft = nums[leftIdx];
+    prevRight = nums[rightIdx];
+    leftIdx++;
+    rightIdx--;
   }
 
   return true;
