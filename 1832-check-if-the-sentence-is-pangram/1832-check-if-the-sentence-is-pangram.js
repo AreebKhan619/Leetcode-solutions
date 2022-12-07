9e35d69c398c9ec2fa1430f39bff447988768627
Areeb Khan <39133435+AreebKhan619@users.noreply.github.com>
2022-12-08 01:59:56 +0530

    Time: 105 ms (36.81%), Space: 42.6 MB (58.03%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
@@ -2,44 +2,17 @@
  * @param {string} sentence
  * @return {boolean}
  */
-var checkIfPangram = function(sentence) {
- const alph = {
-     a: true,
-     b: true,
-     c: true,
-     d: true,
-     e: true,
-     f: true,
-     g: true,
-     h: true,
-     i: true,
-     j: true,
-     k: true,
-     l: true,
-     m: true,
-     n: true,
-     o: true,
-     p: true,
-     q: true,
-     r: true,
-     s: true,
-     t: true,
-     u: true,
-     v: true,
-     w: true,
-     x: true,
-     y: true,
-     z: true
- }  
- const arr = new Array(26)
- let idx = 0;
- while(arr.length && idx < sentence.length){
-     if(alph[sentence[idx]]){
-         delete alph[sentence[idx]]
-         arr.length = arr.length - 1;
-     }
-     idx++;
- }
-    
- return !arr.length;
+var checkIfPangram = function (sentence) {
+  const alphArr = new Array(26).fill(true)
+  const arr = new Array(26)
+  let idx = 0, currIdx = 0
+  while (arr.length && idx < sentence.length) {
+    currIdx = sentence[idx].charCodeAt(0) - 97;
+    if(alphArr[currIdx]){
+      alphArr[currIdx] = false;
+      arr.length--
+    }
+    idx++;
+  }
+  return !arr.length;
 };
